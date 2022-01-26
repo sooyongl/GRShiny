52d46d7825d6fe997b0aa9e1312c2bf53358bed7
Sooyong Lee <sooyongl09@utexas.edu>
2022-01-25 20:47:00 -0600

    Update test_3.r

--- test/test_3.r
+++ test/test_3.r
@@ -3,23 +3,73 @@ for (i in fs::dir_ls("R")) {source(i)}
 
 orddata <- fread("test/paper_data/BYI_DEMO.DAT")[,-c(1:4)]
 
-write_csv(orddata, "test/paper_data/grm_dt.csv")
+# write_csv(orddata, "test/paper_data/grm_dt.csv")
 
 lav.model <- genLavSyn(orddata)
 cat(lav.model)
 
-a1 <- runGRM(dat = orddata, lav.syntax = lav.model, estimator = "ML")
-coef(a1$mirt.fit, simplify = T)
-mirt_out_cleaning(a1$mirt.fit)
+aa1 <- runGRM(dat = orddata, lav.syntax = lav.model, estimator = "ML")
+coef(aa1$mirt.fit, simplify = T, IRTpars = F)$items
+coef(aa1$mirt.fit, simplify = T, IRTpars = T)$items
+output_cleaning(a1)
 a1$grm.par
 
+a1 <- runGRM(orddata, lav.model, "WL")
+summary(a1$lav.fit)
+output_cleaning(a1)
+a1$grm.par
+
+fs_scores <- calFS(a1)
+font_size = 16
+ICCplot(fit = a1, selected_item = 1:2, theta = seq(-6, 6, .1),
+        plot.ps = FALSE,font_size)
+itemplot(a1$mirt.fit, 2)
+plot(
+  a1$mirt.fit,
+  type = 'trace',
+  which.items = 1:2,
+  theta_lim = c(-4, 4),
+  facet_items = FALSE
+)
+
+ICCplot(a1,1:2,seq(-6, 6, .1), plot.ps = T, font_size, addlabel = F)
+ICCplot(a1,1:2,seq(-6, 6, .1), plot.ps = T, font_size, addlabel = T)
+
+
+ESplot(a1,selected_item = 1,seq(-4, 4, .1))
+plot(a1$mirt.fit,type = 'score',
+     which.items = 1,
+     theta_lim =c(-4,4), facet_items = FALSE)
+
+infoPlot(a1,selected_item = 1,
+         theta = seq(-4, 4, .1),
+         type = "icc")
+plot(
+  a1$mirt.fit,
+  type = 'infotrace',
+  which.items = 1,
+  theta_lim = c(-4, 4),
+  facet_items = FALSE
+)
+plot(
+  a1$mirt.fit,
+  type = 'info',
+  which.items = 1,
+  theta_lim = c(-4, 4),
+  facet_items = FALSE
+)
 
 
 
-a2 <- runGRM(orddata, lav.model, "WL")
-summary(a2$lav.fit)
-lavaan_out_cleaning(a2$lav.fit)
-a2$grm.par
 
+infoPlot(a1,type = "tcc",theta = seq(-4, 4, .1))
 
-
+FSplot(a1,type = "histogram",hist_bins = 20)
+FSplot(a1,type = "density",hist_bins = 20)
+plot(
+  a1$mirt.fit,
+  type = 'EAPsum',
+  which.items = 1,
+  theta_lim = c(-4, 4),
+  facet_items = FALSE
+)
