4f4961e43a5e1ec42eb19278117b832305960153
Sooyong Lee <sooyongl09@utexas.edu>
2022-01-28 10:22:34 -0600

    update NAMESPACE

--- R/0_import.r
+++ R/0_import.r
@@ -1,6 +1,6 @@
-#' @import shiny shiny themes shinyWidgets shinydashboard bslib
+#' @import shiny shinythemes shinyWidgets shinydashboard bslib
 #' @import DT
-#' @import lavaan mirt sirt MplusAutomation
+#' @import lavaan mirt sirt
 #' @import ggplot2 dplyr readr tidyr
 #' @importFrom data.table fread fwrite
 #' @importFrom MASS mvrnorm
